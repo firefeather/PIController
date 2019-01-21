d60d061f0a0a7101da35955357a565ae994003a1
Quan <>
2019-01-21 11:01:01 +0800

    temp

--- tools/weiBoSender.py
+++ tools/weiBoSender.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from pyquery import PyQuery as pq
-from jieba import analyse
 
 
 browser = webdriver.Chrome()
