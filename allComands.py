513b2caa980774b5f0b35cd9226e30594b16ab71
quan's pi <manager@dengquan.top>
2019-03-24 08:28:20 +0800

    fix

--- allComands.py
+++ allComands.py
@@ -26,7 +26,7 @@ ALL_COMANDS = [
    Command(name='任务详情',func='获取定时任务的详细情况',usage='任务详情',parmas=None,permission=PERMISSION_LEVEL['SUPER']),
    Command(name='执行任务',func='立即执行指定的定时任务',usage='执行任务:name=xxx',parmas='DIC',permission=PERMISSION_LEVEL['SUPER']),
    Command(name='说话',func='让树莓派通过音响将指定文字说出来',usage='说话:XXX',parmas='STR',permission=PERMISSION_LEVEL['ADMIN']),
-   Command(name='查看日志',func='查看指定日期日志文件(格式YYYY-MM-DD)',usage='查看日志:X-X-X',parmas='STR/NONE',permission=PERMISSION_LEVEL['SUPER'],
+   Command(name='查看日志',func='查看指定日期日志文件(格式YYYY-MM-DD)',usage='查看日志:X-X-X',parmas='STR/NONE',permission=PERMISSION_LEVEL['SUPER']),
    Command(name='重启系统',func='拉取最新代码并重启控制器',usage='重启系统',parmas=None,permission=PERMISSION_LEVEL['SUPER']),
    Command(name='小米众筹',func='获取小米优品上最新的众筹产品信息',usage='小米众筹',parmas=None,permission=PERMISSION_LEVEL['NORMAL']),
    Command(name='设备信息',func='获取当前服务器的状态信息',usage='设备信息',parmas=None,permission=PERMISSION_LEVEL['ADMIN']),
