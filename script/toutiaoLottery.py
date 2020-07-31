bb4458b06d0474417fd5339c8665b3a1f9ff3750
Quan <>
2020-07-31 10:57:20 +0800

    [M]更换头条抽奖cookie

--- script/toutiaoLottery.py
+++ script/toutiaoLottery.py
@@ -27,8 +27,8 @@ SYS_INFO={
 		"SDKVersion": "1.19.3",
 		"errMsg": "getSystemInfo:ok"
 }
-COOKIE='odin_tt=31f588632af88e558675b71d6fc454eb3c7b0ee29b5340b2a0f6d3518b746b80237743aaa0e65410fd861047a9a9745b; UM_distinctid=16ab9241a930-017271cba8078b-5e0e767f-43113-16ab9241a9514b; sid_guard=e80cc24b3a494be29ecd88158eea5985%7C1559488803%7C5184000%7CThu%2C+01-Aug-2019+15%3A20%3A03+GMT; uid_tt=82a7d24c03110699b11a06e6aaf85ed8; sid_tt=e80cc24b3a494be29ecd88158eea5985; sessionid=e80cc24b3a494be29ecd88158eea5985; qh[360]=1; _ga=GA1.2.1073382445.1560087757; _gid=GA1.2.1247773645.1560087757; install_id=74662641983; ttreq=1$8252efe4ae79501b1c9697a42d5bf9985705c321'
 
+COOKIE='odin_tt=1313a030eb3119e5511889fee693319a6b1b28989f6a5675cc79b09c88044fdd7d68a7f3d3c92d0d43d150c49a40c13e; UM_distinctid=171cfedbe801b2-001d3a7dc6925a-53716a3c-448e0-171cfedbe8174; uid_tt=3a02bbbd68b6590b8834103684ace876; sid_tt=f22eedbba6526244303c201cb9a9b966; sessionid=f22eedbba6526244303c201cb9a9b966; WIN_WH=360_701; uid_tt_ss=3a02bbbd68b6590b8834103684ace876; sessionid_ss=f22eedbba6526244303c201cb9a9b966; tt_webid=6838452588007654926; PIXIEL_RATIO=3; qh[360]=1; sid_guard=f22eedbba6526244303c201cb9a9b966%7C1595406445%7C5184000%7CSun%2C+20-Sep-2020+08%3A27%3A25+GMT; install_id=3834692842032852; ttreq=1$9a38750839db1171f71c5ff22f50f18c0c90cf78; history=alrvlFic6pJZXJCTWBmSmZt6KV45U2qpZ0lqbvE9BoHLDKyftCb2NvBfEg5jOXA5Xjm%2B4wQDE8cldg4GB5Cc8CzlAt5LfAIgOaVVP5sbmFguCU5hcLgcrzRRfakDE%2B8lrh4QTzn%2Bo1lTE%2F8lUQmwXCnn0VYmjkusbmDeiV3vmJr4Londgpip8vndFf5LzC0sB0A8k0dBDDyXOE6BbTiVzO3UxHuJxw%2Bs73B1HAMT3yX%2BS2AbMgRamZq4L0nMYQi4HK8YnXrZgYnnEgMDWK5%2Bs03DIb5LUnEgOaWjumkNTNyXGA8xOAAAAAD%2F%2Fw%3D%3D'
 
 def getBigLotteryList():
    headers={
@@ -45,15 +45,18 @@ def getBigLotteryList():
       if not lotteryList is None:
           unJoinList = list(filter(lambda lottery: lottery['JoinState'] != 'JOIN', lotteryList)) or []
           successCount = 0
-          for item in unJoinList:
-            result = joinLotery(item['ActivityNo'])
-            if result == 1:
-                successCount+=1
-            elif result == 3:
-                Logger.v('今日头条全民大抽奖完成,共成功参与{}次抽奖'.format(successCount))
-                break
-            else:
-                continue
+          if len(unJoinList) == 0:
+              Logger.e('今日头条全民大抽奖','没有未参加的大抽奖项目')
+          else:
+             for item in unJoinList:
+                result = joinLotery(item['ActivityNo'])
+                if result == 1:
+                    successCount+=1
+                elif result == 3:
+                    Logger.v('今日头条全民大抽奖完成,共成功参与{}次抽奖'.format(successCount))
+                    break
+                else:
+                    continue
       else:
           Logger.e('今日头条全民大抽奖失败','未获取到大抽奖列表')
    else:
